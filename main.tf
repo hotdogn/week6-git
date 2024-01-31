c79d97fe7753b85aa9ae114f5f955506ae67da79
Barack <baryekee@gmail.com>
2024-01-31 18:21:19 -0500

    lightsail

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ user_data = <<-EOF
               sudo apt-get install -y apache2
               sudo systemctl start apache2
               sudo systemctl enable apache2
-              echo '<h1>This is deployed by Serge </h1>' | sudo tee /var/www/html/index.html
+              echo '<h1>This is deployed by Barack </h1>' | sudo tee /var/www/html/index.html
               EOF
   
 }
